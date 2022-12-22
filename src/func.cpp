b3a018200110f9fbebbe6d4687f47fd799787be8
seanbaccas <seanbaccas@gmail.com>
2022-12-22 23:27:49 +0000

    removed multithreading

--- src/func.cpp
+++ src/func.cpp
@@ -4,57 +4,14 @@
 #include "../include/complex.hpp"
 
 //threading
-#include <thread>
-#include <boost/asio.hpp>
-#include <mutex>
+// #include <thread>
+// #include <boost/asio.hpp>
+// #include <mutex>
 
-const int Concurrency = std::thread::hardware_concurrency();
+// const int Concurrency = std::thread::hardware_concurrency();
 // const int Concurrency = 1;
-std::mutex mutex;
-boost::asio::thread_pool pool(Concurrency);
-
-
-void includeFunction(){
-  std::cout << "printing inside included function\n";
-}
-
-void drawCircle(SDL_Renderer* aRenderer, int32_t aX, int32_t aY, int32_t aR){
-  assert(aRenderer);
-  SDL_SetRenderDrawColor(aRenderer, 0xFF, 0, 0, 255);
-  const int32_t lDiam = 2 * aR;
-  int32_t x = (aR - 1);
-  int32_t y = 0;
-  int32_t tx = 1;
-  int32_t ty = 1;
-  int32_t error = (tx - lDiam);
-  while (x >= y)
-  {
-    //  Each of the following renders an octant of the circle
-    SDL_RenderDrawPoint(aRenderer, aX + x, aY - y);
-    SDL_RenderDrawPoint(aRenderer, aX + x, aY + y);
-    SDL_RenderDrawPoint(aRenderer, aX - x, aY - y);
-    SDL_RenderDrawPoint(aRenderer, aX - x, aY + y);
-    SDL_RenderDrawPoint(aRenderer, aX + y, aY - x);
-    SDL_RenderDrawPoint(aRenderer, aX + y, aY + x);
-    SDL_RenderDrawPoint(aRenderer, aX - y, aY - x);
-    SDL_RenderDrawPoint(aRenderer, aX - y, aY + x);
-
-    if (error <= 0)
-    {
-      ++y;
-      error += ty;
-      ty += 2;
-    }
-
-    if (error > 0)
-    {
-      --x;
-      tx += 2;
-      error += (tx - lDiam);
-    }
-  }
-  SDL_RenderPresent(aRenderer);
-}
+// std::mutex mutex;
+// boost::asio::thread_pool pool(Concurrency);
 
 
 StopWatch::StopWatch( std::string message="")
@@ -76,18 +33,7 @@ StopWatch::~StopWatch()
   std::cout << "+-----------------+\n";
 	}
 
-void lineRender(SDL_Renderer* aRenderer, std::vector<bool>& aMandel, int& aDisplacement) // vector of bools for if we should colour in or not
-{
-  mutex.lock();
-  int lIter = 0;
-  for (auto el : aMandel) 
-  {
-    if (el) SDL_RenderDrawPoint(aRenderer, aDisplacement, lIter);
-    lIter++;
-  }
-  SDL_RenderPresent(aRenderer);
-  mutex.unlock();
-}
+
 
 void blockRender(SDL_Renderer* aRenderer, std::vector<std::vector<bool>>& aScreen, const int aScreenWidth, const int aScreenHeight, double PIXELSCALEFACTOR)
 {
@@ -116,35 +62,35 @@ void mandelDraw ( SDL_Renderer* aRenderer, const int aScreenWidth, const int aSc
     lScaledWidth, std::vector<bool>(lScaledHeight) 
   );
 
-  const double dW = Concurrency * numWidth / double(lScaledWidth) ;  //local variable for width and its increment
+  const double dW = numWidth / double(lScaledWidth) ;  //local variable for width and its increment
   const double dH = numWidth / double(lScaledHeight);
 
-  for (int offset = 0; offset < Concurrency; offset++)
-  // the below will be posted into the threadpool
-  {
-    boost::asio::post( pool, [&, offset](){
+
+    
+    
+    
 
     //lambda starts here -----------------------------------------------------------------------------
 
-      std::vector<bool> lMandelHeightVect (lScaledHeight);
-      double lH;
-      double lW = centre.first -0.5 * numWidth +  dW * offset / Concurrency; //ensures correct starting point
-      for (int widthIter = offset; widthIter < lScaledWidth; widthIter += Concurrency, lW += dW)
-        {
-          lH = centre.second -0.5 * numWidth;
-          for (int heightIter = 0; heightIter < lScaledHeight; heightIter++, lH += dH)
-          {
-            lScreen[widthIter][heightIter] =  isMandelBrot(Complex{lW, lH}, aMaxIters);;
-          }
-        };
-    
+  std::vector<bool> lMandelHeightVect (lScaledHeight);
+  double lH;
+  double lW = centre.first -0.5 * numWidth ;//+  dW * offset / Concurrency; //ensures correct starting point
+  for (int widthIter = 0; widthIter < lScaledWidth; widthIter++, lW += dW)
+    {
+      lH = centre.second -0.5 * numWidth;
+      for (int heightIter = 0; heightIter < lScaledHeight; heightIter++, lH += dH)
+      {
+        lScreen[widthIter][heightIter] =  isMandelBrot(Complex{lW, lH}, aMaxIters);;
+      }
+    }
+
     //lambda ends here -------------------------------------------------------------------------------
 
-      }
-    );
-  }
+      
+    
+  
 
-  pool.join();
+  // pool.join();
 
   std::cout << " Rendering\n";
 
